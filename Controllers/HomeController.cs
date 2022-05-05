31f9ebf0803ebaece5fb41dbc61534048592e618
unknown <padmasrikakumanu9@gmail.com>
2022-05-05 21:57:57 +0530

    commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using AssignmentDay6MvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentDay6MvcApp.Controllers
+{
+    public class HomeController : Controller
+       
+    {
+        static List<User> users = new List<User>();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssignmentDay6MvcApp.MvcApplication" Language="C#" %>
